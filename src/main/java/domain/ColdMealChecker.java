87119315fadc97f29b88cfc82825c3f780cec328
bflessel <bertillon.flessel@gmail.com>
2019-01-23 15:07:56 +0100

    Improving tests for multiple checkins

--- src/main/java/domain/ColdMealChecker.java
+++ src/main/java/domain/ColdMealChecker.java
@@ -8,7 +8,7 @@ public class ColdMealChecker {
 
     public static int count(List<CheckIn> checkIns) {
         return Long.valueOf(checkIns.stream()
-                .filter(checkIn -> checkIn.isPlanned(20, 23, DayOfWeek.THURSDAY))
+                .filter(checkIn -> checkIn.isPlanned(21, 23, DayOfWeek.THURSDAY))
                 .count())
                 .intValue();
     }
--- src/test/java/ColdMealTest.java
+++ src/test/java/ColdMealTest.java
@@ -12,19 +12,31 @@ public class ColdMealTest {
 
     @Test
     public void should_return_a_cold_meal_between_21_and_midnight() {
-        List<CheckIn> coldMealCheckin = Arrays.asList(new CheckIn(DayOfWeek.THURSDAY, "22h00"));
-        assertThat(ColdMealChecker.count(coldMealCheckin)).isEqualTo(1);
+        List<CheckIn> coldMealCheckin = Arrays.asList(
+                new CheckIn(DayOfWeek.THURSDAY, "22h00"),
+                new CheckIn(DayOfWeek.THURSDAY, "23h59"),
+                new CheckIn(DayOfWeek.THURSDAY, "21h00")
+        );
+        assertThat(ColdMealChecker.count(coldMealCheckin)).isEqualTo(3);
     }
 
     @Test
     public void should_return_no_cold_meal_before_21() {
-        List<CheckIn> hotMealCheckin = Arrays.asList(new CheckIn(DayOfWeek.THURSDAY, "19h00"));
+        List<CheckIn> hotMealCheckin = Arrays.asList(
+                new CheckIn(DayOfWeek.THURSDAY, "19h00"),
+                new CheckIn(DayOfWeek.THURSDAY, "17h00"),
+                new CheckIn(DayOfWeek.THURSDAY, "20h59")
+        );
         assertThat(ColdMealChecker.count(hotMealCheckin)).isEqualTo(0);
     }
 
     @Test
     public void should_return_no_cold_meal_after_00() {
-        List<CheckIn> hotMealCheckin = Arrays.asList(new CheckIn(DayOfWeek.FRIDAY, "01h00"));
+        List<CheckIn> hotMealCheckin = Arrays.asList(
+                new CheckIn(DayOfWeek.FRIDAY, "01h00"),
+                new CheckIn(DayOfWeek.FRIDAY, "00h00"),
+                new CheckIn(DayOfWeek.SATURDAY, "23h00")
+        );
         assertThat(ColdMealChecker.count(hotMealCheckin)).isEqualTo(0);
     }
 }
