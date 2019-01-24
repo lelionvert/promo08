0e4047921aa61fce465ea9c4f2292ad281de58bc
massbath <clement.vassant@gmail.com>
2019-01-24 10:31:24 +0100

    Change signature of method to get the number of checkins for a time slot given

--- src/main/java/com/lacombe/socrates/fr/domain/Checkins.java
+++ src/main/java/com/lacombe/socrates/fr/domain/Checkins.java
@@ -1,22 +1,21 @@
 package com.lacombe.socrates.fr.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.time.DayOfWeek.THURSDAY;
-
-public class CheckinRepository {
+public class Checkins {
 
     public static final LocalTime BEGIN_EVENING = LocalTime.of(21, 00);
     private List<CheckIn> checkIns;
 
-    public CheckinRepository(CheckIn... checkIns) {
+    public Checkins(CheckIn... checkIns) {
         this.checkIns = Arrays.asList(checkIns);
     }
 
 
-    public int getColdMeals() {
-        return (int) checkIns.stream().filter(checkIn -> checkIn.isInTimeSlot(THURSDAY, BEGIN_EVENING)).count();
+    public long getNumberOfCheckinsFor(DayOfWeek day, LocalTime beginSlotTime) {
+        return checkIns.stream().filter(checkIn -> checkIn.isInTimeSlot(day, beginSlotTime)).count();
     }
 }
--- src/test/java/com/lacombe/socrates/fr/ColdMealTest.java
+++ src/test/java/com/lacombe/socrates/fr/ColdMealTest.java
@@ -1,7 +1,7 @@
 package com.lacombe.socrates.fr;
 
 import com.lacombe.socrates.fr.domain.CheckIn;
-import com.lacombe.socrates.fr.domain.CheckinRepository;
+import com.lacombe.socrates.fr.domain.Checkins;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Test;
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 
 import java.time.LocalTime;
 
+import static com.lacombe.socrates.fr.domain.Checkins.BEGIN_EVENING;
 import static java.time.DayOfWeek.FRIDAY;
 import static java.time.DayOfWeek.THURSDAY;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,16 +19,16 @@ public class ColdMealTest {
 
     @Test
     public void given_no_checkin_should_have_no_cold_meals() {
-        CheckinRepository checkinRepository = new CheckinRepository();
-        int numberOfMeals = checkinRepository.getColdMeals();
+        Checkins checkins = new Checkins();
+        long numberOfMeals = checkins.getNumberOfCheckinsFor(THURSDAY, BEGIN_EVENING);
         assertThat(numberOfMeals).isEqualTo(0);
     }
 
     @Test
     public void given_a_checking_on_thursday_should_give_1_cold_meal() {
         CheckIn checkInOnThursdayEvening = new CheckIn(THURSDAY, LocalTime.of(21, 00));
-        CheckinRepository checkinRepository = new CheckinRepository(checkInOnThursdayEvening);
-        int numberOfMeals = checkinRepository.getColdMeals();
+        Checkins checkins = new Checkins(checkInOnThursdayEvening);
+        long numberOfMeals = checkins.getNumberOfCheckinsFor(THURSDAY, BEGIN_EVENING);
         assertThat(numberOfMeals).isEqualTo(1);
     }
 
@@ -35,8 +36,8 @@ public class ColdMealTest {
     public void given_a_checking_on_thursday_and_on_another_day_should_give_1_cold_meal() {
         CheckIn checkInOnThursdayEvening = new CheckIn(THURSDAY, LocalTime.of(21, 00));
         CheckIn checkInOnFriday = new CheckIn(FRIDAY, LocalTime.of(21, 00));
-        CheckinRepository checkinRepository = new CheckinRepository(checkInOnThursdayEvening, checkInOnFriday);
-        int numberOfMeals = checkinRepository.getColdMeals();
+        Checkins checkins = new Checkins(checkInOnThursdayEvening, checkInOnFriday);
+        long numberOfMeals = checkins.getNumberOfCheckinsFor(THURSDAY, BEGIN_EVENING);
         assertThat(numberOfMeals).isEqualTo(1);
     }
 
@@ -44,32 +45,32 @@ public class ColdMealTest {
     @Parameters({"21,00", "22,15", "23,30"})
     public void given_a_checking_on_thursday_evening_should_give_1_cold_meal(int hour, int minutes) {
         CheckIn checkInOnThursdayEvening = new CheckIn(THURSDAY, LocalTime.of(hour, minutes));
-        CheckinRepository checkinRepository = new CheckinRepository(checkInOnThursdayEvening);
-        int numberOfMeals = checkinRepository.getColdMeals();
+        Checkins checkins = new Checkins(checkInOnThursdayEvening);
+        long numberOfMeals = checkins.getNumberOfCheckinsFor(THURSDAY, BEGIN_EVENING);
         assertThat(numberOfMeals).isEqualTo(1);
     }
 
     @Test
     public void given_a_checking_before_thursday_evening_should_give_0_cold_meal() {
         CheckIn checkInOnThursdayEvening = new CheckIn(THURSDAY, LocalTime.of(19, 00));
-        CheckinRepository checkinRepository = new CheckinRepository(checkInOnThursdayEvening);
-        int numberOfMeals = checkinRepository.getColdMeals();
+        Checkins checkins = new Checkins(checkInOnThursdayEvening);
+        long numberOfMeals = checkins.getNumberOfCheckinsFor(THURSDAY, BEGIN_EVENING);
         assertThat(numberOfMeals).isEqualTo(0);
     }
 
     @Test
     public void given_a_checking_before_the_end_of_thursday_evening_should_give_1_cold_meal() {
         CheckIn checkInOnThursdayEvening = new CheckIn(THURSDAY, LocalTime.of(23, 59));
-        CheckinRepository checkinRepository = new CheckinRepository(checkInOnThursdayEvening);
-        int numberOfMeals = checkinRepository.getColdMeals();
+        Checkins checkins = new Checkins(checkInOnThursdayEvening);
+        long numberOfMeals = checkins.getNumberOfCheckinsFor(THURSDAY, BEGIN_EVENING);
         assertThat(numberOfMeals).isEqualTo(1);
     }
 
     @Test
     public void given_a_checking_after_the_end_of_thursday_evening_should_give_0_cold_meal() {
         CheckIn checkInOnThursdayEvening = new CheckIn(FRIDAY, LocalTime.of(00, 00));
-        CheckinRepository checkinRepository = new CheckinRepository(checkInOnThursdayEvening);
-        int numberOfMeals = checkinRepository.getColdMeals();
+        Checkins checkins = new Checkins(checkInOnThursdayEvening);
+        long numberOfMeals = checkins.getNumberOfCheckinsFor(THURSDAY, BEGIN_EVENING);
         assertThat(numberOfMeals).isEqualTo(0);
     }
 }
