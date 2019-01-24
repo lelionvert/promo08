d037c0fc864cef45db953d50bbf4b141dc0f1e41
Anas <anas.essaghir@gmail.com>
2019-01-24 15:22:19 +0100

    Initialize project and first test

---
+++ SocratesRegistration/SocratesRegistration/Accommodation.cs
@@ -0,0 +1,22 @@
+﻿namespace SocratesRegistration
+{
+    internal class Accommodation
+    {
+        private Choice choice;
+        
+        public Accommodation(Choice choice)
+        {
+            this.choice = choice;
+        }
+
+        public int Price => (int)choice;
+
+        public enum Choice
+        {
+            Single = 610,
+            Double = 510,
+            Triple = 410,
+            NoAccommodation = 240
+        }
+    }
+}
---
+++ SocratesRegistration/SocratesRegistration/RegistrationTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static SocratesRegistration.Accommodation;
+
+namespace SocratesRegistration
+{
+    [TestClass]
+    public class RegistrationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Accommodation accommodation=new Accommodation(Choice.NoAccommodation);
+            Assert.AreEqual(accommodation.Price,240);
+        }
+    }
+}
