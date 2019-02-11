67ee48b7a273f206aa487c53da68d2c483d826f6
Anas <anas.essaghir@gmail.com>
2019-02-11 09:58:00 +0100

    Test with an expression and no operator done

--- RPNTest/RpnTest.cs
+++ RPNTest/RpnTest.cs
@@ -5,13 +5,20 @@ using RPN;
 namespace RPNTest
 {
     [TestClass]
-    public class UnitTest1
+    public class RpnTest
     {
         [TestMethod]
         public void Given_a_simple_value_should_return_the_value()
         {
             string resultExpected="1";
             Assert.AreEqual(resultExpected,Rpn.Calculate("1"));
+        }
+
+        [TestMethod]
+        public void Given_an_expression_with_no_operator_should_return_the_same()
+        {
+            string resultExpected="1 2";
+            Assert.AreEqual(resultExpected,Rpn.Calculate("1 2"));
         }
     }
 }
