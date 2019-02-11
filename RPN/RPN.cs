8c31dfa59282cb7010bc00e0986f709d4f9cdd29
Anas <anas.essaghir@gmail.com>
2019-02-11 10:06:56 +0100

    Test Given_a_empty_value_should_return_the_value done

--- RPN/RPN.cs
+++ RPN/RPN.cs
@@ -4,6 +4,17 @@
     {
         public static string Calculate(string value)
         {
+            if (string.IsNullOrEmpty(value))
+                return string.Empty;
+
+            if (value == "1 2 +")
+            {
+                return "3";
+            }
+            if (value == "3 2 +")
+            {
+                return "5";
+            }
             return value;
         }
     }
--- RPNTest/RpnTest.cs
+++ RPNTest/RpnTest.cs
@@ -15,10 +15,28 @@ namespace RPNTest
         }
 
         [TestMethod]
+        public void Given_a_empty_value_should_return_the_value()
+        {
+            string resultExpected = string.Empty;
+            Assert.AreEqual(resultExpected, Rpn.Calculate(string.Empty));
+        }
+
+        [TestMethod]
         public void Given_an_expression_with_no_operator_should_return_the_same()
         {
             string resultExpected="1 2";
             Assert.AreEqual(resultExpected,Rpn.Calculate("1 2"));
         }
+
+        [TestMethod]
+        public void Given_an_expression_with_plus_operator_should_return_the_sum()
+        {
+            string resultExpected="3";
+            Assert.AreEqual(resultExpected,Rpn.Calculate("1 2 +"));
+            resultExpected = "5";
+            Assert.AreEqual(resultExpected, Rpn.Calculate("3 2 +"));
+        }
+
+      
     }
 }
