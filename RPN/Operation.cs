e409fc1be7e480946a63ea79cc6503c43910e0f1
Anas <anas.essaghir@gmail.com>
2019-02-12 10:42:02 +0100

    Refactoring and add dictionnary to handle operations

--- RPN/Operation.cs
+++ RPN/Operation.cs
@@ -1,10 +1,14 @@
-﻿namespace RPN
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPN
 {
     public abstract class Operation
     {
         protected const string PlusSymbol = "+";
         protected const string MinusSymbol = "-";
         protected const string DivideSymbol = "/";
+
         protected readonly int _firstOperand;
         protected readonly int _secondOperand;
         public static OperationBuilder Builder=new OperationBuilder();
@@ -27,8 +31,17 @@
             private int _firstOperand;
             private int _secondOperand;
             private string _symbol;
+            private readonly IReadOnlyDictionary<string, Func<int, int, Operation>> _operations;
 
-            internal OperationBuilder() {}
+            internal OperationBuilder()
+            {
+                _operations = new Dictionary<string, Func<int, int, Operation>>
+                {
+                    {PlusSymbol, (x, y) => new AdditionOperation(x, y)},
+                    {MinusSymbol, (x, y) => new SusbstrationOperation(x, y)},
+                    {DivideSymbol, (x, y) => new DivisionOperation(x, y)}
+                };
+            }
 
             public OperationBuilder WithFirstOperand(int firstOperand)
             {
@@ -50,14 +63,10 @@
 
             public Operation Build()
             {
-                if (_symbol == DivideSymbol)
-                    return new DivisionOperation(_firstOperand, _secondOperand);
-                if (_symbol == MinusSymbol)
-                    return new SusbstrationOperation(_firstOperand, _secondOperand);
+                if (_operations.ContainsKey(_symbol))
+                    return _operations[_symbol](_firstOperand,_secondOperand);
 
                 return new AdditionOperation(_firstOperand, _secondOperand);
-
-                
             }
         }
     }
